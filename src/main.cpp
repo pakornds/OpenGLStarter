1331a6817e220761a3183bd67ed5e40a3759bb31
pakornds <pakorn.tantiwutthiphat@gmail.com>
2025-09-16 14:45:54 +0700

    Initial commit: OpenGLStarter (CMake + vcpkg)

---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+#undef GLFW_DLL
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <vector>
+#include <cmath>
+
+#include "Libs/Shader.h"
+#include "Libs/Window.h"
+#include "Libs/Mesh.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+//Vertex Shader
+static const char* vShader = "Shaders/shader.vert";
+
+//Fragment Shader
+static const char* fShader = "Shaders/shader.frag";
+
+void CreateTriangle()
+{
+    // GLfloat vertices[] =
+    // {
+    //     -1.0f, -1.0f, 0.0f,
+    //     1.0f, -1.0f, 0.0f,
+    //     0.0f, 1.0f, 0.0f
+    // };
+
+    GLfloat vertices[] =
+    {
+    -1.0f, -1.0f, 0.0f,
+    0.0f, -1.0f, 1.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f
+    };
+    // unsigned int indices[] =
+    // {
+    //     0, 1, 2,
+    // };
+
+    // Mesh *obj1 = new Mesh();
+    // obj1->CreateMesh(vertices, indices, 9, 9);
+    // meshList.push_back(obj1);
+
+    unsigned int indices[] =
+    {
+    0, 3, 1,
+    1, 3, 2,
+    2, 3, 0,
+    0, 1, 2
+    };
+    Mesh *obj1 = new Mesh();
+    obj1->CreateMesh(vertices, indices, 12, 12);
+    meshList.push_back(obj1);
+
+    // Second object - Triangle
+    GLfloat triangleVertices[] =
+    {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    unsigned int triangleIndices[] =
+    {
+        0, 1, 2
+    };
+    Mesh *obj2 = new Mesh();
+    obj2->CreateMesh(triangleVertices, triangleIndices, 9, 3);
+    meshList.push_back(obj2);
+}
+
+void CreateShaders()
+{
+    Shader* shader1 = new Shader();
+    shader1->CreateFromFiles(vShader, fShader);
+    shaderList.push_back(*shader1);
+}
+
+int main()
+{
+    mainWindow = Window(WIDTH, HEIGHT, 3, 3);
+    mainWindow.initialise();
+
+    CreateTriangle();
+    CreateShaders();
+
+    GLuint uniformModel = 0, uniformProjection = 0;
+
+    glm::mat4 projection = glm::perspective(45.0f,
+(GLfloat)mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(),
+0.1f, 100.0f);
+
+    //Loop until window closed
+    while (!mainWindow.getShouldClose())
+    {
+        //Get + Handle user input events
+        glfwPollEvents();
+
+        //Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //draw here
+        shaderList[0].UseShader();
+
+        uniformModel = shaderList[0].GetUniformLocation("model");
+        uniformProjection = shaderList[0].GetUniformLocation("projection");
+        //Object
+        glm::mat4 model (1.0f);
+        
+        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
+        // model = glm::translate(model, glm::vec3(0.3f, 1.0f, -2.5f));
+        model = glm::rotate(model, 90.0f * 3.1416f / 180.0f, glm::vec3(0.0f,
+        0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+        // model = glm::scale(model, glm::vec3(2.0f, 2.0f, 1.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+
+        meshList[0]->RenderMesh();
+
+        // Second object - Triangle (positioned to the left)
+        glm::mat4 model2 (1.0f);
+        model2 = glm::translate(model2, glm::vec3(-1.0f, 0.5f, -2.0f));
+        model2 = glm::rotate(model2, 45.0f * 3.1416f / 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+        model2 = glm::scale(model2, glm::vec3(0.3f, 0.3f, 1.0f));
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model2));
+        meshList[1]->RenderMesh();
+
+        glUseProgram(0);
+        //end draw
+
+        mainWindow.swapBuffers();
+    }
+
+    return 0;
+}
